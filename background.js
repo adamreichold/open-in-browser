7bcaae14d4796496503fcc30afbf8b8e1fb4d528
Adam Reichold <adam.reichold@t-online.de>
2024-07-19 16:54:15 +0200

    Require messagesUpdate permission to become compatible with Thunderbird 128.

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 /*
 
-Copyright 2021-2022 Adam Reichold
+Copyright 2021-2022, 2024 Adam Reichold
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
